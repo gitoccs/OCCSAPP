9897c897bd71a2f343e78b105bd8cfb3297de78b
occsApp <liqiuze@occs.com.cn>
2016-01-16 17:48:18 +0800

    modify1

--- OCCS-android/app/src/main/java/com/occs/ldsoft/occs/StartActivity.java
+++ OCCS-android/app/src/main/java/com/occs/ldsoft/occs/StartActivity.java
@@ -9,7 +9,7 @@ public class StartActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
 
-        //开始的入口
+        //开始的入口测试
         return new StartFragment();
     }
 }
